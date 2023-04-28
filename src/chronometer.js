6bf17f6a6f8abb294e6ca01b08754f5e7aff57d0
Achref LAABIDI <abidi.achref95@gmail.com>
2023-04-28 16:45:57 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,51 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      printTimeCallback(this.currentTime)
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const timeInMn = this.currentTime / 60
+    let minutes = Math.floor(timeInMn)
+    if (minutes < 1) {
+      return 0
+    } else {
+      return minutes
+    }
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return String(value).padStart(2, '0')
   }
 
   stop() {
-    // ... your code goes here
+      clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    const timeInMn = this.currentTime / 60
+    let minutes = Math.floor(timeInMn)
+    const mResult = minutes.toString().padStart(2, '0')
+    let seconds = this.currentTime % 60
+    const sResult = seconds.toString().padStart(2, '0')
+
+    return String(`${mResult}:${sResult}`)
   }
 }
